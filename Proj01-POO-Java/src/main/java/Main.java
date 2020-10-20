b8c9a50b1233b1a91ad7992cd815ff56da585a43
ortizfabioh <50219525+ortizfabioh@users.noreply.github.com>
2020-10-20 17:02:54 -0300

    Update Main.java

--- Proj01-POO-Java/src/main/java/Main.java
+++ Proj01-POO-Java/src/main/java/Main.java
@@ -1,15 +1,10 @@
-import com.sun.xml.internal.ws.util.StringUtils;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Leitura dados = new Leitura();
         
-        String arquivo = "ex2";
+        String arquivo = "ex4";
         
         String texto = dados.pegarLinhas("src/main/java/exercicios/"+arquivo);
         String textoTratado = dados.tratarTexto(texto);
