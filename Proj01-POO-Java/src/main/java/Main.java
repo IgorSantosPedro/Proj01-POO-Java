30613ec0cc2130d57e2c4ac7174c3e7867d8e5a6
IgorSantosPedro <igorpedrosantos21@gmail.com>
2020-10-31 16:03:20 -0300

    Main.java

--- Proj01-POO-Java/src/main/java/Main.java
+++ Proj01-POO-Java/src/main/java/Main.java
@@ -4,13 +4,29 @@ public class Main {
     public static void main(String[] args) {
         Leitura dados = new Leitura();
         Ordena ordena = new Ordena();
+        CriarGrafo listaPalavra = new CriarGrafo();
         
         String arquivo = "ex3";
         
-        String texto = dados.pegarLinhas("src/main/java/exercicios/"+arquivo);
+        String texto = dados.pegarLinhas("exercicios/"+arquivo);
         String textoTratado = dados.tratarTexto(texto);
         
         String[] textoTratadoSplit = ordena.separar(textoTratado);
+        
+        String[][] lista = CriarGrafo.listar(textoTratadoSplit);
+        String[][] proximo = CriarGrafo.proximo(lista);
+        String[][] elimina = CriarGrafo.repeti(proximo);
+        String[][] novo = CriarGrafo.nova(elimina);
+        
+        for (int l = 0; l < novo.length; l++)  {  
+            for (int c = 0; c < novo[0].length; c++){
+            	if(novo[l][c] != "") {
+            		System.out.print(novo[l][c] + " "); //imprime caracter a caracter
+            	}
+            }  
+            System.out.println(" "); //muda de linha
+          }  
+        
         String[] arr = ordena.removerPalavrasRepetidasNoArray(textoTratadoSplit);
         
         /*
@@ -18,6 +34,12 @@ public class Main {
         */
         
         String[] arr1 = ordena.ordenarOrdemAlfabeto(arr);
-        ordena.printArray(arr1);
+        
+        
     }
+
+	private static String toString(String x) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
