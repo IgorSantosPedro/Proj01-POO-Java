f70c0373f8739decc133ec4e2249ce861f32906b
ortizfabioh <50219525+ortizfabioh@users.noreply.github.com>
2020-11-04 07:31:15 -0300

    Update

--- Proj01-POO-Java/src/main/java/Main.java
+++ Proj01-POO-Java/src/main/java/Main.java
@@ -14,7 +14,7 @@ public class Main {
         
         String arquivo = "ex4";
         
-        String texto = dados.pegarLinhas("src/main/java/exercicios/"+arquivo);
+        String texto = dados.pegarLinhas("java/exercicios/"+arquivo);
         String textoTratado = dados.tratarTexto(texto);
         String[] textoTratadoSplit = dados.separar(textoTratado);
         
